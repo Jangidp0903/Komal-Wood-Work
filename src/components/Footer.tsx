efaa162a83f977be4f0f71ca13bde45ed3f3fb41
Jangidp0903 <156504199+Jangidp0903@users.noreply.github.com>
2025-07-30 12:53:01 +0530

    Update Time in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,9 +25,9 @@ const Footer: React.FC = memo(() => {
 
   const businessHours = useMemo(
     () => [
-      { day: "Mon - Fri", hours: "9:00 AM - 6:00 PM" },
-      { day: "Saturday", hours: "9:00 AM - 6:00 PM" },
-      { day: "Sunday", hours: "9:00 AM - 6:00 PM" },
+      { day: "Mon - Fri", hours: "9:00 AM - 9:00 PM" },
+      { day: "Saturday", hours: "9:00 AM - 9:00 PM" },
+      { day: "Sunday", hours: "9:00 AM - 9:00 PM" },
     ],
     []
   );
